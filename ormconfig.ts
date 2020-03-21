b2b0178f496a9fdb950f3f6dfcf805d3e2001f0f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-03-21 22:20:46 +1100

    added database and password

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,9 +1,10 @@
-const { DB_HOST, DB_PASS } = process.env;
+const { DB, DB_HOST, DB_PASS } = process.env;
 
 module.exports = {
   name: 'default',
   type: 'postgres',
   host: DB_HOST,
   username: 'postgres',
-  
+  database: DB,
+  password: DB_PASS,
 }
