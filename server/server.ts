aa0be63d096bfcbc7a2cd699c71a5b8e3d70fbed
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-03-24 21:08:05 +1100

    created entry server file

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import passport from 'passport';
+import passportLocal from 'passport-local';
+import 'reflect-metadata';
+import databaseService from './services/DatabaseService';
+import { User } from './models/user';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from './repositories/UserRepository';
+import initializePassport from './controllers/authController';
+
+const LocalStrategy = passportLocal.Strategy;
+
+initializePassport(passport, email => {
+  
+}) 
 
 const app = express();
 
@@ -10,6 +23,9 @@ app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server is running on port ${ port }`);
+  console.log('Initialising database...');
+  await databaseService.initDB();
+  console.log('Database connected')
 })
