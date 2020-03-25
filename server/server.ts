08de6dc93e2689a6a2733d1bbb9b70da85336cae
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-03-25 20:01:36 +1100

    added passport

--- server/server.ts
+++ server/server.ts
@@ -1,31 +1,77 @@
 import express from 'express';
 import cors from 'cors';
 import passport from 'passport';
-import passportLocal from 'passport-local';
+import { Strategy } from 'passport-local';
 import 'reflect-metadata';
 import databaseService from './services/DatabaseService';
-import { User } from './models/user';
+import userRoutes from './routes/userRoutes';
+import initializePassport from './config/passportConfig';
+import session from 'express-session';
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from './repositories/UserRepository';
-import initializePassport from './controllers/authController';
-
-const LocalStrategy = passportLocal.Strategy;
-
-initializePassport(passport, email => {
-  
-}) 
+import { User } from './models/User';
 
 const app = express();
+
+passport.use(
+  new Strategy(
+    { usernameField: "email" },
+    async (email: string, password: string, done: any) => {
+      const authError: Error = new Error("Email or Password is incorrect");
+
+      const emailError: Error = new Error("User does not exist");
+
+      const user = await getCustomRepository(UserRepository).findByEmail(email);
+
+      if (user == null) {
+        return done(emailError);
+      }
+
+      try {
+        if (user && user.comparePassword(password)) {
+          return done(null, user);
+        } else {
+          return done(authError);
+        }
+      } catch (e) {
+        return done(e);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user: User, done: any) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((user: User, done: any) => {
+  done(
+    null,
+    getCustomRepository(UserRepository).findOne({
+      where: { id: user.id }
+    })
+  );
+});
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'S3cret',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/user', userRoutes);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
   console.log(`Server is running on port ${ port }`);
-  console.log('Initialising database...');
+  console.log("Initializing database...");
   await databaseService.initDB();
-  console.log('Database connected')
-})
+  console.log("Database connected");
+});
