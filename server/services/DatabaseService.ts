8ab8893170d8eac6997c8014cfdedd545f559ec5
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-03-25 20:08:39 +1100

    added getCurrentConnection

--- server/services/DatabaseService.ts
+++ server/services/DatabaseService.ts
@@ -3,7 +3,7 @@ import debug from 'debug';
 
 const error = debug('database.service');
 const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 5432;
+const DB_PORT = process.env.DB_PORT || '5432';
 const DB = process.env.DB || 'blue';
 const DB_USER = process.env.DB_USER || 'postgres';
 const DB_PASS = process.env.DB_PASS || 'postgres';
@@ -11,7 +11,11 @@ const DB_PASS = process.env.DB_PASS || 'postgres';
 class DatabaseService {
   private connection: Connection;
 
-  async initDB(sync = true): Promise<any> {
+  getCurrentConnection(): Connection {
+    return this.connection;
+  }
+
+  async initDB(sync = false): Promise<any> {
     try {
       const options: any = {
         type: 'postgres',
@@ -30,6 +34,7 @@ class DatabaseService {
       if (!this.connection) {
         this.connection = await createConnection(options);
       }
+
     } catch (e) {
       error(e.message);
       process.exit(1);
